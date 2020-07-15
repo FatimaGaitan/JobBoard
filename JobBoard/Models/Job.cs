717c9e22c0b980f35fdb9b83a3d1bfa3b2467876
Fátima Gaitán <faty_gaitan@hotmail.com>
2020-07-14 20:38:12 -0600

    Commit

---
+++ JobBoard/Models/Job.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JobBoard.Models
+{
+    public class Job
+    {
+        [Key]
+        public int Job_ID { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string Job_Title { get; set; }
+        [Required]
+        [StringLength(350)]
+        public string Description { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        [Required]
+        public DateTime ExpiresAt { get; set; }
+    }
+}
